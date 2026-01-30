a90c27f695328558ebce0557bce38ac7d4627955
paritosh ramanan <paritosh.ramanan@okstate.edu>
2026-01-30 12:27:55 -0600

    networking done right

--- networking/app/main.py
+++ networking/app/main.py
@@ -1,8 +1,15 @@
 import streamlit as st
 
 st.title("Hi from streamlit inside docker")
-st.write("looks like this works properly.")
-st.write("What happens if you change the file?")
 
-st.text("Please enter your name:")
-st.text_input("Name")
+def submit_name():
+    st.session_state.submitted = True
+
+st.text_input(
+    "Please enter your name:",
+    key="name",
+    on_change=submit_name
+)
+
+if st.session_state.get("submitted", False):
+    st.write(f"My name is: {st.session_state.name}")
